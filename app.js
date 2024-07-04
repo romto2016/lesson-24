90a88003a5f46ec3d83fd091e54cc3a884a0987b
Roman <romto2016@gmail.com>
2024-07-03 23:17:02 -0500

    lesson-25, add window and close button

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-let productCountEl = document.getElementById("product-count");
-let addToCardBtns = document.querySelectorAll(".button-section-cart");
-// console.log(productCountEl);
-// console.log(addToCardBtns);
+// let productCountEl = document.getElementById("product-count");
+// let addToCardBtns = document.querySelectorAll(".button-section-cart");
+// // console.log(productCountEl);
+// // console.log(addToCardBtns);
 
-addToCardBtns.forEach((btn) =>
-  btn.addEventListener("click", function () {
-    // console.log("clicked");
-    // document.getElementById("product-count").innerHTML++;
-    productCountEl.textContent = +productCountEl.textContent + 1;
-  })
-);
-// ===========================================================
-let pressBtn = document.querySelectorAll(".heart-button");
-console.log(pressBtn);
-// for (i = 0; i < pressBtn.length; i++) {
-//   pressBtn[i].addEventListener("click", function () {
-//     //     // console.log("clicked");
-//     console.log("liked");
-//   });
-// }
-pressBtn.forEach((btn) =>
-  btn.addEventListener("click", function () {
-    if (btn.classList.contains("liked")) {
-      btn.classList.remove("liked");
-    } else {
-      btn.classList.add("liked");
-    }
-    // btn.classList.toggle("liked");
-    // console.log("licked");
-  })
-);
+// addToCardBtns.forEach((btn) =>
+//   btn.addEventListener("click", function () {
+//     // console.log("clicked");
+//     // document.getElementById("product-count").innerHTML++;
+//     productCountEl.textContent = +productCountEl.textContent + 1;
+//   })
+// );
+// // // ===========================================================
+// let pressBtn = document.querySelectorAll(".heart-button");
+// console.log(pressBtn);
+// // for (i = 0; i < pressBtn.length; i++) {
+// //   pressBtn[i].addEventListener("click", function () {
+// //     //     // console.log("clicked");
+// //     console.log("liked");
+// //   });
+// // }
+// pressBtn.forEach((btn) =>
+//   btn.addEventListener("click", function () {
+//     if (btn.classList.contains("liked")) {
+//       btn.classList.remove("liked");
+//     } else {
+//       btn.classList.add("liked");
+//     }
+//     // btn.classList.toggle("liked");
+//     // console.log("licked");
+//   })
+// );
 
 // const btn2 = document.querySelectorAll(".header-button");
 // btn2.disabled = true;
@@ -67,27 +67,35 @@ pressBtn.forEach((btn) =>
 //   inputFields.value = newValue;
 //   countDisabledState(newValue);
 // });
-// add slider
-
-// $(".slider-wrapper").slick({
-//   adaptiveHeigth: true,
-//   dots: true,
-// });
-// let moreDetailsButtons = document.querySelectorAll(".button-section-details");
-// const modal = document.querySelector(".modal");
-// console.log(moreDetailsButtons);
-// console.log(modal);
-// moreDetailsButtons.forEach((btn) =>
-//   btn.addEventListener("click", function () {
-//     // modal.style.display = "block";
-//     modal.classList.add("show");
-//   })
-// );
-
-// const btnClose = document.querySelector(".btn-close");
 // console.log(btnClose);
 // btnClose.forEach((btn) =>
 //   btn.addEventListener("click", function () {
 //     btnClose.style.display = "none";
 //   })
 // );
+
+
+// ==================================================================================
+// add slider
+
+$(".slider-wrapper").slick({
+  adaptiveHeigth: true,
+  dots: true,
+});
+let moreDetailsButtons = document.querySelectorAll(".button-section-details");
+const modal = document.querySelector(".modal");
+console.log(moreDetailsButtons);
+console.log(modal);
+moreDetailsButtons.forEach((btn) =>
+  btn.addEventListener("click", function () {
+    // modal.style.display = "block";
+    modal.classList.add("show");
+  })
+);
+
+const closeBtn = document.querySelector(".btn-close");
+
+closeBtn.addEventListener("click", function () {
+  modal.classList.remove("show");
+});
+
